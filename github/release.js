307b76d40de76db180593895f1f089deb7c50763
Pip Cet <pipcet@gmail.com>
2020-06-19 08:51:40 +0000

    minor log additions

--- github/release.js
+++ github/release.js
@@ -1,10 +1,13 @@
 const this_release_date = process.argv[2];
 const last_release_date = process.argv[3];
 const child_process = require("child_process");
-const log = child_process.execSync(`git log ${last_release_date}..HEAD --pretty=format:'%H %s%b'`).toString();
+const log = child_process.execSync(`git log ${last_release_date}..HEAD --pretty=format:'%H %s%b'`).toString() || "no changes";
+const last_hash = child_process.execSync(`git rev-parse ${last_release_date}`).toString();
 let json = {};
 json.tag_name = this_release_date;
 json.name = `this_release_date (automatic release)`;
 json.prelease = true;
-json.body = log;
+let body = `Changes since ${last_release_date} (${last_hash}):\n\n`;
+body += log;
+json.body = body;
 console.log(JSON.stringify(json));
