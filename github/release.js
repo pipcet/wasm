adc6a594a9254fa23cbb5ecf3c9418929c744b8f
Pip Cet <pipcet@gmail.com>
2020-06-21 01:07:19 +0000

    github/release.js: did I mention JavaScript is hard?

--- github/release.js
+++ github/release.js
@@ -4,9 +4,11 @@ let last_release_date = process.argv[3];
 //last_release_date = last_release_date.substr(1, this_release_date.length - 2)
 let child_process = require("child_process");
 let log = child_process.execSync(`git log ${last_release_date}..HEAD --pretty=format:'%H %s'`).toString() || "no changes";
-let body = child_process.execSync(`git log ${last_release_date}..HEAD --pretty=format:'%b'`).toString();
-if (body)
-    log += "\n" + body + "\n";
+{
+    let body = child_process.execSync(`git log ${last_release_date}..HEAD --pretty=format:'%b'`).toString();
+    if (body)
+	log += "\n" + body + "\n";
+}
 let last_hash = child_process.execSync(`git rev-parse ${last_release_date}`).toString();
 while (last_hash.length && last_hash[last_hash.length-1] === "\n")
     last_hash = last_hash.substr(0, last_hash.length-1);
