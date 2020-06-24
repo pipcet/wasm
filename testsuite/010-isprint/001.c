96b2efbe393691c67378913c04199368f2e42fcd
Pip Cet <pipcet@gmail.com>
2020-06-24 19:40:35 +0000

    test the test test

--- testsuite/010-isprint/001.c
+++ testsuite/010-isprint/001.c
@@ -4,7 +4,5 @@
 
 int main(void)
 {
-  uselocale (LC_GLOBAL_LOCALE);
-  printf ("%p\n", uselocale (LC_GLOBAL_LOCALE)->__ctype_b);
-  printf ("%d\n", isprint_l('a', uselocale (LC_GLOBAL_LOCALE)));
+  printf ("%d\n", isprint('a'));
 }
