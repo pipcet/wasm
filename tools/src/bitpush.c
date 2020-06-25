9704a4b6647ba721da31541392da774ccf005cd6
Pip Cet <pipcet@gmail.com>
2020-06-25 20:21:17 +0000

    tools/src/bitpush.c: kill subsidiary process when exiting

--- tools/src/bitpush.c
+++ tools/src/bitpush.c
@@ -16,6 +16,8 @@ int main(int argc, char **argv)
   asprintf (&outname, "%s/in", argv[1]);
   char *inname;
   asprintf (&inname, "%s/out", argv[1]);
+  char *pidname;
+  asprintf (&inname, "%s/pid", argv[1]);
   int ofd = open (inname, O_RDONLY | O_NONBLOCK);
 
   while (1) {
@@ -25,10 +27,21 @@ int main(int argc, char **argv)
     };
 
     poll (pfd, 2, -1);
-    if (pfd[0].revents & POLLERR)
-      exit (1);
-    if (pfd[1].revents & POLLERR)
-      exit (1);
+    if ((pfd[0].revents & POLLERR)
+	|| (pfd[1].revents & POLLERR))
+      {
+	fprintf (stderr, "ouch, dying\n");
+	int fd;
+	fd = open (pidname, O_RDONLY);
+	if (fd >= 0) {
+	  char buf[512];
+	  ssize_t n = read (fd, buf, 512);
+	  buf[n] = 0;
+	  pid_t pid = atoi(buf);
+	  kill(pid, SIGKILL);
+	  exit(1);
+	}
+      }
     if (pfd[0].revents & POLLIN) {
       char buf[512];
       ssize_t n = read (0, buf, sizeof buf);
