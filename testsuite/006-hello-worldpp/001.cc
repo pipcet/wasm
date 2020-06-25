4d34d0cfa31bf2f0ac7d25b2d430d20d0a1cde94
Pip Cet <pipcet@gmail.com>
2020-06-25 23:16:22 +0000

    testsuite/006-hello-worldpp/001.cc: rm dead code

--- testsuite/006-hello-worldpp/001.cc
+++ testsuite/006-hello-worldpp/001.cc
@@ -1,11 +1,6 @@
 #include <iostream>
 
-//asm(".include \"wasm32-import-macros.s\"");
-//asm(".import3_pic thinthin,recopy,__thinthin_recopy");
-//extern "C" long __thinthin_recopy(void);
-
 int main()
 {
-  //__thinthin_recopy();
   std::cout << "hello world++\n";
 }
