87fe032a75ba8b7a76fccddf30c3ac51f68f5b0d
Pip Cet <pipcet@gmail.com>
2020-06-25 08:59:53 +0000

    testsuite/006-hello-worldpp/001.cc: comments

--- testsuite/006-hello-worldpp/001.cc
+++ testsuite/006-hello-worldpp/001.cc
@@ -1,6 +1,11 @@
 #include <iostream>
 
+//asm(".include \"wasm32-import-macros.s\"");
+//asm(".import3_pic thinthin,recopy,__thinthin_recopy");
+//extern "C" long __thinthin_recopy(void);
+
 int main()
 {
+  //__thinthin_recopy();
   std::cout << "hello world++\n";
 }
