de6b546a3d471dd16bc3c2271deb96557d8ccc2e
Pip Cet <pipcet@gmail.com>
2021-01-23 16:49:09 +0000

    try node after all

--- github/env/index.js
+++ github/env/index.js
@@ -1,3 +1,4 @@
-console.log(`::set-env name=ACTIONS_RUNTIME_URL::${process.env.ACTIONS_RUNTIME_URL}`)
-console.log(`::set-env name=ACTIONS_RUNTIME_TOKEN::${process.env.ACTIONS_RUNTIME_TOKEN}`)
-console.log(`::set-env name=GITHUB_RUN_ID::${process.env.GITHUB_RUN_ID}`)
+const fs = require("fs");
+fs.appendFile(process.env.GITHUB_ENV, `ACTIONS_RUNTIME_URL=${process.env.ACTIONS_RUNTIME_URL}`);
+fs.appendFile(process.env.GITHUB_ENV, `ACTIONS_RUNTIME_TOKEN=${process.env.ACTIONS_RUNTIME_TOKEN}`);
+fs.appendFile(process.env.GITHUB_ENV, `GITHUB_RUN_ID=${process.env.GITHUB_RUN_ID}`);
