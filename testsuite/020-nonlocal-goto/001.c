28e9e28a449ed92071226a83778da4ba69b1ac80
Pip Cet <pipcet@gmail.com>
2021-01-28 12:52:49 +0000

    testsuite/020-nonlocal-goto/001.c: expand test

--- testsuite/020-nonlocal-goto/001.c
+++ testsuite/020-nonlocal-goto/001.c
@@ -1,16 +1,26 @@
+#include <stdio.h>
+
 int main(void)
 {
   __label__ out;
   volatile int x0 = 7;
+  printf("x0 at %p\n", &x0);
   int f(int x)
   {
+    volatile int y0 = 3;
+    printf("before non-local jump\n");
     if (x && x0)
-      return x0 + x;
-    goto out;
+      goto out;
+    printf("after non-local jump\n");
+    return x0 + x;
   }
   if (f(8))
     return 0;
 
+  printf("falling through\n");
  out:
+  printf("received non-local jump\n");
+  printf("x0 at %p\n", &x0);
+  printf("x0 = %d\n", x0);
   return 1;
 }
