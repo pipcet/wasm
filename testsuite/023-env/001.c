7ea518d2eb52e107a31b7c971cf6b3bf9cb3ac07
Pip Cet <pipcet@gmail.com>
2021-01-30 20:10:07 +0000

    testsuite/023-env/001.c: expand test

--- testsuite/023-env/001.c
+++ testsuite/023-env/001.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv, char **envp)
 {
+  printf("environ %p %p\n", __environ, &__environ);
   while (*envp) {
     printf("%s\n", *envp++);
   }
