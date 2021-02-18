7378d6a26f144a7344dedec8da4f7e0186c3735b
Pip Cet <pipcet@gmail.com>
2021-02-18 20:23:02 +0000

    github/dl-artifact.js: minor fix

--- github/dl-artifact.js
+++ github/dl-artifact.js
@@ -3,7 +3,7 @@ const client = artifact.create();
 const name = process.argv[2];
 async function main()
 {
-    console.log(await client.downloadArtifact(name, `artifacts/${name}.new`, {
+    console.log(await client.downloadArtifact(name, `artifacts/down/${name}.new`, {
 	continueOnError: false,
 	createArtifactFolder: false,
     }));
