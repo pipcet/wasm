6ee67eb32641ed6667e9df00325fe861744fa962
Pip Cet <pipcet@gmail.com>
2022-01-04 09:26:59 +0000

    testsuite/005-__thinthin_write/001.c: apparently, something changed about static symbols

--- testsuite/005-__thinthin_write/001.c
+++ testsuite/005-__thinthin_write/001.c
@@ -1,5 +1,7 @@
 asm(".include \"wasm32-import-macros.s\"");
-//asm(".import3 thinthin,write,__thinthin_write");
+#ifdef SHARED
+asm(".import3 thinthin,write,__thinthin_write");
+#endif
 extern void __thinthin_write(int, char *, int) __attribute__((stackcall));
 
 int main(void)
