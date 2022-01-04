774e0bb4e65d3bcb6d4bfb4219e86350dd6d7eb7
Pip Cet <pipcet@gmail.com>
2022-01-04 10:11:24 +0000

    testsuite/025-hello-worldpp-with-explicit-recopy/001.cc: "fix" test

--- testsuite/025-hello-worldpp-with-explicit-recopy/001.cc
+++ testsuite/025-hello-worldpp-with-explicit-recopy/001.cc
@@ -1,8 +1,15 @@
 #include <iostream>
 
+#ifndef SHARED
+#define SHARED /* currently, the static libstdc++ isn't being built. */
+#endif
+
 extern "C" {
   asm(".include \"wasm32-import-macros.s\"");
   extern void __thinthin_recopy(void) __attribute__((stackcall));
+#ifdef SHARED
+  asm(".import3 thinthin,recopy,__thinthin_recopy");
+#endif
 };
 
 int main()
